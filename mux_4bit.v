60fc0b384323426368dbdebba1ff7915ebf95aac
rgladdis <61604936+rgladdis@users.noreply.github.com>
2020-05-14 15:52:36 -0700

    MUX!

---
+++ mux_4bit.v
@@ -0,0 +1,15 @@
+
+module mux2( d0, d1, s, y );
+input [3:0] d0, d1;
+input s;
+output [3:0] y;
+reg [3:0] y;
+
+always@(d0 or d1 or s)
+begin
+    if ( s == 1 )
+        y = d1;
+    else
+        y = d0;
+end
+endmodule
